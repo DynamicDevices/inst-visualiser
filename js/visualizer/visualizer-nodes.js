3d5a81b606c7d91a8e72d35d996e2d6e7865b939
JenLaing <jen@nellyvision.com>
2025-07-31 22:05:27 +0100

    pipeline test

--- js/visualizer/visualizer-nodes.js
+++ js/visualizer/visualizer-nodes.js
@@ -147,9 +147,11 @@ class VisualizerNodeManager {
     updateNodeContent(node, nodeId) {
         if (!node.element) return;
         
-        const displayText = (nodeId.length > 4 ? nodeId.substring(0, 4) : nodeId) + "0"; //(node.gps ? `( ${node.gps.lat.toFixed(2)}, ${node.gps.lng.toFixed(2)})` : 'no GPS');
+        const displayText = (nodeId.length > 3 ? nodeId.substring(0, 3) : nodeId);
         node.element.textContent = displayText;
     }
+
+    //(node.gps ? `( ${node.gps.lat.toFixed(2)}, ${node.gps.lng.toFixed(2)})` : 'no GPS');
 
     /**
      * Update node appearance based on state
