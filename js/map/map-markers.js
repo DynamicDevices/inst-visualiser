34031936dfdf4ed37753cf32580dd71f885e5ac4
JenLaing <jen@nellyvision.com>
2025-08-01 00:29:45 +0100

    increasing dp to 10 on lat/lon to see if they are actually different

--- js/map/map-markers.js
+++ js/map/map-markers.js
@@ -178,7 +178,7 @@ class MapMarkerManager {
                     border: 1px solid ${styling.color};
                     white-space: nowrap;
                     text-align: center;
-                ">${gpsCoords.lat.toFixed(2)}, ${gpsCoords.lng.toFixed(2)}
+                ">${gpsCoords.lat.toFixed(10)}, ${gpsCoords.lng.toFixed(10)}
                 </div>
                 <div style="
                     position: absolute;
